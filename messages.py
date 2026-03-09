8cc103b5223d64687e4cf7cf4770c5df5607cf85
Mateo Levesque <levesquemateo@gmail.com>
2026-03-09 02:24:00 -0400

    clean up

--- messages.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from textual.message import Message
-
-
-class TasksUpdated(Message):
-    def __init__(self, date: date, tasks: list[str]):
-        self.date = date
-        self.tasks = tasks
-        super().__init__()
